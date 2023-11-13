a4e2070c431133f3339438de92e87b8f22707091
Roseane Aires <134559171+anneaires@users.noreply.github.com>
2023-11-13 19:35:15 -0300

    Add project

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/style.css" />
+	<title>PROGRAMACAO WEB</title>
+</head>
+
+<body>
+	<nav class="navbar">
+		<!-- LOGO -->
+		<div class="logo">CRUD USUÁRIOS</div>
+
+		<!-- NAVIGATION MENU -->
+		<ul class="nav-links">
+			<!-- NAVIGATION MENUS -->
+			<div class="menu">
+
+				<li><a href="index.php">Home</a></li>
+				<li><a href="?page=novo">Novo usuário</a></li>
+				<li><a href="?page=listar">Listar usuário</a></li>
+				<li><a href="?page=buscar">Buscar um usuário</a></li>
+			</div>
+		</ul>
+	</nav>
+
+	<div class="container">
+		<div class="row">
+			<div class="col mt-5">
+				<?php
+					include("config.php");
+					switch (@$_REQUEST["page"]) {
+						case "novo":
+							include("novo-usuario.php");
+							break;
+						case "listar":
+							include("listar-usuario.php");
+							break;
+						case "buscar":
+							include("buscar-usuario.php");
+							break;
+						case "utils":
+							include("utils-usuario.php");
+							break;
+						case "editar":
+							include("editar-usuario.php");
+							break;
+						default:
+							print("<h1>Bem vindo!</h1>");
+					}
+				?>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
